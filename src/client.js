332cca8de1754a1fa12efb207e881bb5e91c06d6
Peter van Dijk <petervandijk@gmail.com>
2018-12-11 15:37:58 +0100

    webpack scss support

--- src/client.js
+++ src/client.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import "./css/style.css";
+import "./sass/main.scss";
 // import {Provider} from 'react-redux'
 // import {BrowserRouter} from 'react-router-dom'
-import App from './App'
+import App from './App';
 // import createStore from './store'
 
 // const store = createStore( window.REDUX_DATA );
