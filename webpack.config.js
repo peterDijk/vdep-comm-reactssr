3b84bfd1b197896e1159a49fcfadadb90bfe85d9
Peter van Dijk <petervandijk@gmail.com>
2018-12-11 15:38:14 +0100

    webpack scss support

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require( "path" );
 const { BundleAnalyzerPlugin } = require( "webpack-bundle-analyzer" );
 const FriendlyErrorsWebpackPlugin = require( "friendly-errors-webpack-plugin" );
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const plugins = [
@@ -11,7 +12,10 @@ const plugins = [
     new CopyWebpackPlugin([
         // relative path is from src
         { from: './images/favicon.ico' }, // <- your path to favicon
-      ])
+      ]),
+    new MiniCssExtractPlugin({
+        filename: 'style.css',
+      })
 ];
 
 if ( !dev ) {
@@ -43,13 +47,9 @@ module.exports = {
                 loader: "babel-loader",
             },
             {
-                test:/\.css$/,
-                use: ExtractTextPlugin.extract(
-                    {
-                      fallback: 'style-loader',
-                      use: ['css-loader']
-                    })
-            }
+                test: /\.scss$/,
+                use:  [  'style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+              }
         ],
     },
     output: {
