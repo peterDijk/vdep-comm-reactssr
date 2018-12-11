5d9ec4f868e25a0b76219e2addea72fb94862667
Peter van Dijk <petervandijk@gmail.com>
2018-12-11 13:06:16 +0100

    basic react component + ssr

---
+++ src/client.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+// import {Provider} from 'react-redux'
+// import {BrowserRouter} from 'react-router-dom'
+import App from './App'
+// import createStore from './store'
+
+// const store = createStore( window.REDUX_DATA );
+
+ReactDOM.hydrate(
+  // <Provider store={store}>
+    // <BrowserRouter>
+      <App />
+    // </BrowserRouter>
+  // </Provider>
+  , document.getElementById('root'));
+  
---
+++ src/server.js
@@ -0,0 +1,44 @@
+import express from "express";
+import path from "path";
+
+import * as React from "react";
+import { renderToString } from "react-dom/server";
+
+import App from "./App";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use( express.static( path.resolve( __dirname, "../dist" ) ) );
+
+app.get( "/*", (req, res) => {
+  
+  const jsx = (
+    <App />
+  );
+  const reactDom = renderToString( jsx );
+
+  res.writeHead( 200, { "Content-Type": "text/html" } );
+  res.end( htmlTemplate( reactDom ) );
+} );
+
+app.listen( PORT, () => {
+  console.log(`server listening on port ${PORT} `);
+})
+
+function htmlTemplate( reactDom ) {
+  return `
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset="utf-8">
+    <title>Communicatie over Grenzen</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+  </head>
+  <body>
+    <div id="root">${ reactDom }</div>
+    <script src="./app.bundle.js"></script>
+  </body>
+  </html>
+  `;
+}
