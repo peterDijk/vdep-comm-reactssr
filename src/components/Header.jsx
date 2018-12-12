68b30f3835b689dc146ac8d2828b3a2b1652c2a8
Peter van Dijk <petervandijk@gmail.com>
2018-12-12 20:48:39 +0100

    styling template

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import * as React from 'react'
 function Header(props) {
   const { items } = props
   return (
-    <header className="header" style={{'--bg-img-url': `url(${props.content.bgImgUrl})`}}>
-      <nav>
+    <header className="header" style={{'--bg-img-url': `url(images/mark-eder-310600-unsplash.jpg)`}}>
+      {/* <nav className="header__nav">
         <ul>
         { items.map(item => {
           return (
@@ -12,17 +12,19 @@ function Header(props) {
           )
         }) }
         </ul>
-      </nav>
+      </nav> */}
       <div className="header__logo-box">
         <img src="images/vdep-logo.png" alt="Van Dijk E-Projects" className="header__logo"/>
       </div>
       <div className="header__text-box">
-        <span className="heading-primairy--main">
-          { props.content.headingMain }
-        </span>
-        <span className="heading-primairy--sub">
-          { props.content.headingSub }
-        </span>
+        <h1>
+          <span className="heading-primary heading-primary--main">
+            { props.content.headingMain }
+          </span>
+          <span className="heading-primary heading-primary--sub">
+            { props.content.headingSub }
+          </span>
+        </h1>
       </div>
 
     </header>
