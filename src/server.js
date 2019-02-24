2dd6b002150c9b8a4ea3b3d975950fae677bbd54
Peter van Dijk <petervandijk@gmail.com>
2019-02-24 22:53:05 +0100

    added analytics script

--- src/server.js
+++ src/server.js
@@ -2,23 +2,12 @@ import express from "express"
 import path from "path"
 
 import * as React from "react"
-import {
-  renderToString
-} from "react-dom/server"
-import {
-  StaticRouter,
-  matchPath
-} from 'react-router-dom'
-import {
-  Provider
-} from 'react-redux'
+import { renderToString } from "react-dom/server"
+import { StaticRouter, matchPath } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import createStore from './store'
-import {
-  reduxInit
-} from './actions/utils'
-import {
-  requestSeminars
-} from './actions/seminars'
+import { reduxInit } from './actions/utils'
+import { requestSeminars } from './actions/seminars'
 
 import routes from './routes'
 
@@ -28,62 +17,50 @@ import App from "./App"
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use(express.static(path.resolve(__dirname, "../dist")))
+app.use( express.static( path.resolve( __dirname, "../dist" ) ) )
 
-app.get("/*", async (req, res) => {
+app.get( "/*", async (req, res) => {
   console.log(req);
-  const context = {}
+  const context = { }
   const store = createStore()
 
-  store.dispatch(reduxInit())
-  await store.dispatch(requestSeminars())
+  store.dispatch( reduxInit() )
+  await store.dispatch( requestSeminars() )
 
   const dataRequirements =
-    routes
-    .filter(route => matchPath(req.url, route)) // filter matching paths
-    .map(route => route.component) // map to components
-    .filter(comp => comp.serverFetch) // check if components have data requirement
-    .map(comp => {
-      return store.dispatch(comp.serverFetch())
-    }); // dispatch data requirement
+        routes
+            .filter( route => matchPath( req.url, route ) ) // filter matching paths
+            .map( route => route.component ) // map to components
+            .filter( comp => comp.serverFetch ) // check if components have data requirement
+            .map( comp => {
+              return store.dispatch( comp.serverFetch( ) )
+             } ); // dispatch data requirement
 
-  Promise.all(dataRequirements).then(() => { // performance?? the render of content only continues after all serverFetches are resolved
+  Promise.all( dataRequirements ).then( () => { // performance?? the render of content only continues after all serverFetches are resolved
 
-    const jsx = ( <
-      Provider store = {
-        store
-      } >
-      <
-      StaticRouter context = {
-        context
-      }
-      location = {
-        req.url
-      } >
-      <
-      App / >
-      <
-      /StaticRouter> <
-      /Provider>
+    const jsx = (
+      <Provider store={ store }>
+        <StaticRouter context={ context } location={ req.url }>
+          <App />
+        </StaticRouter>
+      </Provider>
     );
-    const reactDom = renderToString(jsx)
+    const reactDom = renderToString( jsx )
     const reduxState = store.getState()
 
-    res.writeHead(200, {
-      "Content-Type": "text/html"
-    })
-    res.end(htmlTemplate(reactDom, reduxState))
+    res.writeHead( 200, { "Content-Type": "text/html" } )
+    res.end( htmlTemplate( reactDom, reduxState ) )
 
+  
 
+  } )
+} );
 
-  })
-});
-
-app.listen(PORT, () => {
+app.listen( PORT, () => {
   console.log(`server listening on port ${PORT} `)
 })
 
-function htmlTemplate(reactDom, reduxState) {
+function htmlTemplate( reactDom, reduxState ) {
   return `
   <!DOCTYPE html>
   <html>
@@ -98,6 +75,15 @@ function htmlTemplate(reactDom, reduxState) {
       window.__INITIAL_STATE__ = ${ JSON.stringify( reduxState )}
     </script>
     <script src="/app.bundle.js"></script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-135032110-1"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+
+ gtag('config', 'UA-135032110-1');
+</script>
   </body>
   </html>
   `
