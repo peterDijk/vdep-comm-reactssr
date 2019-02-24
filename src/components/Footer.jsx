6152a42d911c7abcf558307743c1f4a0e0fc85cd
Peter van Dijk <petervandijk@gmail.com>
2019-02-24 21:21:58 +0100

    enable footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,11 @@ import * as React from "react";
 import ReactHtmlParser from "react-html-parser";
 
 export const Footer = props => {
+  console.log(props);
   return (
     <div className="footer">
       <div className="paragraph">
-        {ReactHtmlParser(props.footer.attributes.body.value)}
+        {/* {ReactHtmlParser(props.footer.attributes.body.value)} */}
       </div>
     </div>
   );
